2af6891eeeca55fddaeb40c147f2ab5e827b8bc7
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-09-05 00:48:18 +0300

    Commits

--- models/city.py
+++ models/city.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-City module
-"""
+"""Contains the City model"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """
-    City Module
-    Attributes:
-    state_id(string)
-    name(string)
-    """
+    """Implements the City class"""
     state_id = ""
     name = ""
