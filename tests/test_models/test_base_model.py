2061ce023d12f9b81a6479bc798ecf041970b9be
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-09-04 21:48:54 +0300

    base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Unittest for basemodel module"""
+import models
+import os
 import unittest
+from time import sleep
 from datetime import datetime
 from models.base_model import BaseModel
 
@@ -10,6 +13,9 @@ class TestBaseModel_instantiation(unittest.TestCase):
 
     def test_no_args(self):
         self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_new_instance_in_objects(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
 
     def test_id_str_public(self):
         self.assertEqual(str, type(BaseModel().id))
@@ -54,6 +60,52 @@ class TestBaseModel_instantiation(unittest.TestCase):
         self.assertEqual(bm.updated_at, dt)
 
 
+class TestBaseModel_save(unittest.TestCase):
+    """Testing BaseModel's save method"""
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    def test_single_save(self):
+        bm = BaseModel()
+        sleep(0.05)
+        first = bm.updated_at
+        bm.save()
+        self.assertLess(first, bm.updated_at)
+
+    def test_two_saves(self):
+        bm = BaseModel()
+        sleep(0.05)
+        first = bm.updated_at
+        bm.save()
+        second = bm.updated_at
+        self.assertLess(first, second)
+        sleep(0.05)
+        bm.save()
+        self.assertLess(second, bm.updated_at)
+
+    def test_save_updates_file(self):
+        bm = BaseModel()
+        bm.save()
+        bmid = "BaseModel." + bm.id
+        with open("file.json", "r") as f:
+            self.assertIn(bmid, f.read())
+
+
 class TestBaseModel_to_dict(unittest.TestCase):
     """testing the to_dict method of BaseModel"""
 
@@ -70,7 +122,7 @@ class TestBaseModel_to_dict(unittest.TestCase):
 
     def test_to_dict_with_added_attributes(self):
         bm = BaseModel()
-        bm.name = "My First Model"
+        bm.name = "Holberton"
         self.assertIn("name", bm.to_dict())
 
     def test_contrast_to_dict_dunder_dict(self):
@@ -89,7 +141,6 @@ class TestBaseModel_to_dict(unittest.TestCase):
             'updated_at': dt.isoformat()
         }
         self.assertDictEqual(bm.to_dict(), tdict)
-
 
 if __name__ == "__main__":
     unittest.main()
